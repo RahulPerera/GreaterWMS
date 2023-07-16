d847b85228ae1a9b81250d009e7e4bae0e3ed6d1
RahulPerera <56962897+RahulPerera@users.noreply.github.com>
2023-07-16 12:43:50 +0530

    Update Index.vue

--- templates/src/pages/Index.vue
+++ templates/src/pages/Index.vue
@@ -4,7 +4,7 @@
       <div class="q-mb-xl" style="color: #4C5875;text-align: center;">
         <div style="font-weight: bold;font-size: 100px;letter-spacing: 10px;">WELCOME</div>
         <div style="font-size: 22px;letter-spacing: 2px;">
-          GreaterWMS
+          TLM Logistics
           <span v-if="isEnglish">&nbsp</span>
           {{ $t('index.index_title') }}
         </div>
